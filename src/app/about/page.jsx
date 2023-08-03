2485c7c45d41703deb7f8f5a8f9d7c5b16ad9218
jeremih999 <brownjeremih@gmail.com>
2023-08-03 03:47:57 +0100

    first commit

---
+++ src/app/about/page.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const about = ()=>{
+    return <div className="container">about</div>
+}
+
+export default about
---
+++ src/app/blog/page.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const blog = ()=>{
+    return <div className="container">blog</div>
+}
+
+export default blog
