d1aa5b8e6be42fa3db6f1ad552deb659204a9b6b
jeremih999 <brownjeremih@gmail.com>
2023-10-24 02:41:41 +0100

    still trying

--- src/utils/db.js
+++ src/utils/db.js
@@ -1,11 +1,11 @@
 import mongoose from "mongoose";
 
-async function connect() {
-    try {
-       await mongoose.connect(process.env.MONGO)
-    } catch (error) {
-        throw new Error("Failed to connect to DB")
-    }
-}
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO);
+  } catch (error) {
+    throw new Error("Connection failed!");
+  }
+};
 
-export default connect ;
+export default connect;
